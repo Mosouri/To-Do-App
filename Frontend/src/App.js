f1149b074caa23c9b5816015c49d1c788b5c3968
Mosouri <mohommad.souri@gmail.com>
2026-03-28 23:21:01 +0300

    test

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -1,11 +1,30 @@
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  return (<>
-  <div>
-    <h1>Hello World!</h1>
-  </div>
-  </>)
+  const [tasks, setTasks] = useState([]);
+  const [input, setInput] = useState("");
+
+  return (
+    <div style={{ padding: "20px" }}>
+      <h1>To Do App</h1>
+
+      <input
+        type="text"
+        placeholder="Add task..."
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+
+      <button>Add</button>
+
+      <ul>
+        {tasks.map((task, index) => (
+          <li key={index}>{task}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default App;
